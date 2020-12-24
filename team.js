39b2dfdcd1f4528e93b0ea94006fda323f37b9b5
mathieusalz <mathieusalz1@gmail.com>
2020-12-23 22:58:07 -0500

    Update team.js

--- team.js
+++ team.js
@@ -5,9 +5,41 @@ export default class Team {
         this.teamCards = [];
     }
 
-    addpoints(gained){
+    addPoints(gained){
         this.points += gained
     }
+    
+    // Once round is done, checks if the number of cards the team has won is over 19, and adjusts points accordingly
+    checkOver(){
+        let nc = this.teamCards.numberOfCards
+        if (nc > 19){
+            //If number of cards is odd, add an extra point to round up 
+            let gainedPoints = nc % 2 == 0 ? (nc - 20) + 6: (nc-20) + 7
+            this.points += gainedPoints
+        }
+    }
+
+    // Checks if a players has 3 or 4 of a kind
+    check3or4(){
+        for (let n = 0; n < 2; n ++){
+            
+            let cardList = []
+            for (let i = 0; i < 6; i++){
+            
+                cardList += translate(this.players[n].deck.cards[i])
+          }
+          
+          let ucards = set(cardList)
+          if (6 - ucards.length == 3) {
+            
+            //This should trigger a win
+          } else if (6- uncards.length == 4) {
+            
+            this.points += 2
+          } 
+        }
+      } 
+            
 }
 
 class Players{
