a938fdc74c8236fecaedbe928387d24ca9debb78
GanglyPuma22 <76500958+GanglyPuma22@users.noreply.github.com>
2020-12-29 20:52:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-  
 import Deck from "./deck.js"
+import Card from "./card.js"
+
 const deck = new Deck();
 deck.shuffle();
-console.log(deck.cards);
+console.log(deck);
 
+const imageTest = document.querySelector(".test");
+// const card = new Card("♣","2");
+// console.log(card.suitToText());
+// console.log(card.createHTML());
+
+for (let i = 0; i < deck.numberOfCards; i++) {
+   imageTest.appendChild(deck.cards[i].createHTML());
+}
