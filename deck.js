badfecd86a62a8aa9b0ddab5e78fb570091c53c9
GanglyPuma22 <76500958+GanglyPuma22@users.noreply.github.com>
2020-12-29 20:54:09 -0500

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,18 +1,23 @@
+
 const SUITS = ["♠", "♣", "♥", "♦"]
 
 const VALUES = [
-  "1",
+  "A",
   "2",
   "3",
   "4",
   "5",
   "6",
   "7",
+  "8",
+  "9",
+  "10",
   "J",
   "Q",
   "K"
 ]
 
+import Card from "./card.js" 
 
 export default class Deck {
 	constructor(cards = freshDeck()) {
@@ -33,6 +38,12 @@ export default class Deck {
   }
   
   // This function refills the players deck only if he has less than 6 cards, and removes those added cards from the main deck
+  /*
+   Good practice for functions is to add text that explains what it does, which you added.
+   It is also good to put inputs and outputs so that we can quickly see what the function needs.
+   Inputs: 
+   Outputs: 
+  */
   refill(mainDeck,playerDeck){
     // Initiliaze variable just to make things simpler
     let nc = playerDeck.numberOfCards
@@ -54,6 +65,10 @@ export default class Deck {
   }
   
   //This function checks for a limpia, i.e. the displayed cards are 0, and adjusts the points accordingly
+  /*
+   Inputs: 
+   Outputs: 
+  */
   checkLimpia(team){
     if (this.numberOfCards == 0){
       team.points += 2
@@ -69,14 +84,6 @@ export default class Deck {
 }
 
 
-class Card {
-	constructor(suit, value) {
-		this.suit = suit;
-		this.value = value;
-  }
-
-}
-
 function freshDeck() {
   return SUITS.flatMap(suit => {
     return VALUES.map(value => {
@@ -86,23 +93,36 @@ function freshDeck() {
 }
 
 // Checks for succesful addition, and adjusts points and team cards accordingly
+/*
+   Inputs: 
+   Outputs: 
+*/
 function checkAddition(selectedCards,playerCard,playedDeck,team){
   //Calculate the sum of the cards which are selected
   let sumCards = selectedCards.cards.value.reduce(function(a, b){
     return a + b;
     }, 0);
+    /*
+      It might be worth making a new function here that we should have a generic function updateDeck or maybe two functions 
+      removeFromDeck and addtoDeck so that we can use them in any given situation where we alter decks. This works if the inputs to the function are consistant
+    */
     //If condition met, team cards are adjusted and cards are removed from the played Deck
     if (sumCards == playerCard.value){
      for (let i = 0; i < selectedCards.numberOfCards; i++){
        playedDeck.splice(playedDeck.indexof(selectedCards[i]),1)
        team.teamCards += selectedCards[i]
      } 
-  } else {
-       return false
-     }
+  } //If you have nothing to put in the else statement, you dont have to add it
+    //else {
+       //return false
+     //}
 
   }  
 // This function checks for a Caida, i.e. the value of both cards is the same, and adjust points and team cards accordingly
+/*
+   Inputs: 
+   Outputs: 
+*/
 function checkCaida(card1,card2,team,playedDeck){
   if (card1.value == card2.value){
     team.teamCards += card1 + card2
@@ -113,6 +133,10 @@ function checkCaida(card1,card2,team,playedDeck){
 }
 
 // This function adds cards to the team cards if the cards selected are consecutive and superior  
+/*
+   Inputs: 
+   Outputs: 
+*/
 function checkSlide(card,playedDeck,selectedCards,team){
   let cardList = card
   //Translate the selected cards so that we can compare their values later on
@@ -129,6 +153,10 @@ function checkSlide(card,playedDeck,selectedCards,team){
 }
 
 // Checks to see if the cards are consecutive
+/*
+   Inputs: 
+   Outputs: 
+*/
 function checkConsecutive(cardList){
   // Check if the difference between two consective cards in the array is different than one
   // The cardList parameter should be sorted beforehand, so if condition is met then the cards are not consecutive
@@ -142,6 +170,10 @@ function checkConsecutive(cardList){
 }
 
 // Converts the cards values to integers
+/*
+   Inputs: 
+   Outputs: 
+*/
 function translate(card){
   if (card.value == "J"){
     return 8
