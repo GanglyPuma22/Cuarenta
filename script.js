01b67e10c65ed91a38946e00d0ecadc8b37a88b4
Maxim Mounier <Maxim>
2022-06-16 18:21:31 -0400

    Added drag card functionality and start game visuals

--- script.js
+++ script.js
@@ -1,6 +1,69 @@
 import Deck from "./deck.js"
 import { freshDeck } from "./deck.js";
 
+var deck = new Deck(freshDeck());
+var player1 = new Deck([]);
+deck.shuffle();
+deck.drawCards(5, player1);
+
+const createGameBtn = document.getElementById("choose-game-creation-option").children.item(0);
+const joinGameBtn = document.getElementById("choose-game-creation-option").children.item(1);
+//Handle creating a game
+createGameBtn.addEventListener("click", function() {
+  console.log("Hello there");
+  document.getElementById("game-host").style.display = "none";
+  document.getElementsByClassName("user-hand").item(0).style.display = "block";
+  showCards(player1);
+});
+
+//Handle joining a game
+joinGameBtn.addEventListener("click", function() {
+  console.log("Hello there");
+  document.getElementById("game-host").style.display = "none";
+  document.getElementsByClassName("user-hand").item(0).style.display = "block";
+  showCards(player1);
+});
+
+(function () {
+  let playerId;
+  let playerRef;
+  let players = {};
+  let team1 = {};
+  let team2 = {};
+
+   firebase.auth().onAuthStateChanged((user) => {
+    console.log(user)
+    if (user) {
+      //You're logged in!
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`players/${playerId}`);
+      var playerName = "John";
+
+      playerRef.set({
+        id: playerId,
+        name: playerName
+      })
+
+      //Remove me from Firebase when I diconnect
+      playerRef.onDisconnect().remove();
+      //players.
+      //Begin the game now that we are signed in
+      //initGame();
+    } else {
+      //You're logged out.
+    }
+  })
+
+  //Auth error log 
+  firebase.auth().signInAnonymously().catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode, errorMessage);
+  });
+
+})();
+
 const CARD_VALUE_MAP = {
    "2": 2,
    "3": 3,
@@ -14,21 +77,22 @@ const CARD_VALUE_MAP = {
    "A": 14
 }
 
-var deck = new Deck(freshDeck());
-var player1 = new Deck([]);
-deck.shuffle();
-deck.drawCards(5, player1);
+
 
 
 const imageTest = document.querySelector(".test");
 const userHandDiv = document.querySelector(".user-hand");
-
-for (let i = 0; i < player1.numberOfCards; i++) {
-   //htmlImage = player1.cards[i].createHTML();
-   // htmlImage.addEventListener("click", () => {
-   //    console.log(player1.cards[i].value);
-   //  });
-   userHandDiv.appendChild(player1.cards[i].createHTML()); 
-   
+function showCards(playerdeck) {
+  for (let i = 0; i < playerdeck.numberOfCards; i++) {
+    //htmlImage = player1.cards[i].createHTML();
+    // htmlImage.addEventListener("click", () => {
+    //    console.log(player1.cards[i].value);
+    //  });
+    userHandDiv.appendChild(playerdeck.cards[i].createHTML()); 
+    
+ }
 }
+
+
+
 
