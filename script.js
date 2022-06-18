b689119205ee9f24de181ae6c2e81f21fb7166a6
Maxim Mounier <Maxim>
2022-06-17 23:09:37 -0400

    Expanded on visual functionality and connecting js with firebase

--- script.js
+++ script.js
@@ -6,49 +6,156 @@ var player1 = new Deck([]);
 deck.shuffle();
 deck.drawCards(5, player1);
 
-const createGameBtn = document.getElementById("choose-game-creation-option").children.item(0);
-const joinGameBtn = document.getElementById("choose-game-creation-option").children.item(1);
-//Handle creating a game
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
+
+const createGameBtn = document.querySelector("button[name='create']");
+const joinGameBtn = document.querySelector("button[name='join']");
+const gameSessionBtn = document.querySelector("button[name='create-session'");
+const joinSessionInp = document.querySelector("input[name='join-session'");
+
+//Show option to create a game
 createGameBtn.addEventListener("click", function() {
-  console.log("Hello there");
-  document.getElementById("game-host").style.display = "none";
-  document.getElementsByClassName("user-hand").item(0).style.display = "block";
-  showCards(player1);
+  console.log("hello");
+  document.getElementById("choose-game-creation-option").style.display = "none";
+  document.getElementById("create-game").style.display = "block";
+  //document.getElementsByClassName("user-hand").item(0).style.display = "block";
+  //showCards(player1);
 });
 
-//Handle joining a game
+//Show option to join a game
 joinGameBtn.addEventListener("click", function() {
-  console.log("Hello there");
-  document.getElementById("game-host").style.display = "none";
-  document.getElementsByClassName("user-hand").item(0).style.display = "block";
-  showCards(player1);
+  document.getElementById("choose-game-creation-option").style.display = "none";
+  document.getElementById("join-game").style.display = "block";
+  //document.getElementsByClassName("user-hand").item(0).style.display = "block";
+  //showCards(player1);
 });
+
+
 
 (function () {
   let playerId;
   let playerRef;
+
+  //Use function to create teams in firebase once everyone has joined the game
+  function initGame() {
+    const allPlayersRef = firebase.database().ref(`players`);
+    const allTeamsRef = firebase.database().ref('teams');
+  }
+  
   let players = {};
   let team1 = {};
   let team2 = {};
+  
+  //Show option to create a game session for host
+  gameSessionBtn.addEventListener("click", function() {
+    var gameId = makeid(20);
+    let gameSessionRef = firebase.database().ref('gameSession/'+gameId);
 
-   firebase.auth().onAuthStateChanged((user) => {
-    console.log(user)
+    //Create game session
+    gameSessionRef.set({
+      id: gameId,
+      playerCount: 1
+    });
+    
+    //Add player to game session's players
+    let newPlayerRef = firebase.database().ref(`gameSession/${gameId}/sessionPlayers`);
+    newPlayerRef.push().set({
+      playerId: playerId
+    });
+
+    //Get current game session player count data
+    let playerCountRef = firebase.database().ref('gameSession/'+gameId+'/playerCount');
+
+    //Fires whenever a change occurs
+    playerCountRef.on("value", (snapshot) => {
+      //Update game host's playerCount
+      document.getElementById("player-count").innerHTML = "   Player Count: " + snapshot.val();
+    })
+
+    //Resolve promise 
+    playerCountRef.get().then(function() {
+      return playerCountRef.once("value");
+    }).then(function(snapshot) {
+      //Show key to game session creator and hide button that created game
+      document.getElementById('create-game').style.display = "none";
+      let showCreated = document.getElementById("create-game-id-created");
+      showCreated.style.display = "block";
+      const para1 = document.createElement("p");
+      const para2 = document.createElement("p");
+      const para3 = document.createElement("p");
+      para1.innerHTML = "   Game Session ID is: " + gameId;
+      para2.innerHTML = "   Share your game session ID to three other players. Once all four are connected the game will start.";
+      para3.innerHTML = "   Player Count: " + snapshot.val();
+      para3.setAttribute("id", "player-count");
+      showCreated.appendChild(para1);
+      showCreated.appendChild(para2);
+      showCreated.appendChild(para3);
+      //Start Game
+    });
+
+  });
+
+  joinSessionInp.addEventListener("change", function() {
+    //TODO Improve how we determine the sessionID is valid, right now it just makes sure its the right length for 1
+    if (joinSessionInp.value.length == 20) {
+      let sessionRef = firebase.database().ref('gameSession/'+joinSessionInp.value);
+      let gameSessionJoinRef = firebase.database().ref('gameSession/'+joinSessionInp.value+'/sessionPlayers');
+      let playerCountRef = firebase.database().ref('gameSession/'+joinSessionInp.value+'/playerCount');
+
+      //Resolve promise getting current player count data
+      playerCountRef.get().then(function() {
+        return playerCountRef.once("value");
+      }).then(function(snapshot) {
+      let currentPlayerCount = snapshot.val();
+      //If session isn't full add player to it
+      if (currentPlayerCount < 4) {
+        //Add this player to gameSession's players
+        gameSessionJoinRef.push().set({
+          playerId: playerId
+        });
+        //Update player count
+        sessionRef.update({
+          playerCount: currentPlayerCount + 1
+        })
+      } else {
+        console.log("GAME FULL");
+      }
+
+      if (currentPlayerCount + 1 == 4) {
+        console.log(currentPlayerCount);
+        console.log("START GAME");
+        //Start Game
+      }
+      });
+    }
+    // if (joinSessionInp.value == )
+  });
+
+
+  //After login this function triggers
+  firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       //You're logged in!
       playerId = user.uid;
       playerRef = firebase.database().ref(`players/${playerId}`);
-      var playerName = "John";
+      //var playerName = "John";
 
       playerRef.set({
-        id: playerId,
-        name: playerName
+        id: playerId
       })
 
       //Remove me from Firebase when I diconnect
       playerRef.onDisconnect().remove();
-      //players.
-      //Begin the game now that we are signed in
-      //initGame();
+      firebase.database().ref('gameSession').onDisconnect().remove();
     } else {
       //You're logged out.
     }
@@ -78,12 +185,9 @@ const CARD_VALUE_MAP = {
 }
 
 
-
-
-const imageTest = document.querySelector(".test");
-const userHandDiv = document.querySelector(".user-hand");
 function showCards(playerdeck) {
   for (let i = 0; i < playerdeck.numberOfCards; i++) {
+    const userHandDiv = document.querySelector(".user-hand");
     //htmlImage = player1.cards[i].createHTML();
     // htmlImage.addEventListener("click", () => {
     //    console.log(player1.cards[i].value);
