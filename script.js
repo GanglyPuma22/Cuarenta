fdc32132d484e08f60d4ab7b7abb75e7ffa74dc3
Maxim Mounier <Maxim>
2022-06-19 18:25:14 -0400

    Updated some database stuff

--- script.js
+++ script.js
@@ -44,41 +44,165 @@ joinGameBtn.addEventListener("click", function() {
   let playerRef;
   let boardRef;
   let deckRef;
+  let cardsRef;
   let gameSessionRef;
 
   //Storage vars
   let players = {};
+  let dbDeck = {};
+  let cardNum = 0;
   let team1 = {};
   let team2 = {};
   let playerLimit = 4;
+  let playerTurnID; //Keeps track of player ID who's current turn it is
+
   //Class variables 
-  let deck; 
+  let deck = new Deck(freshDeck()); //Create game deck
+  deck.shuffle(); //Shuffle it
+  let deck1;
+  let deck2;
+  let deck3;
+  let deck4;
+
   let board;
-  let playerHand; //database does not need player hand info
+  let playerHand = new Deck([]); //database does not need player hand info
 
   function createTeams() {
     let counter = 0;
 
     Object.keys(players).forEach((key) => {
-      console.log(players[key].playerId);
+      //console.log(players[key].playerId);
       if (counter == 0) { 
         team1.player1 = players[key];
+        team1.player1.cards = deck1;
         counter++;
       } else if (counter == 1) {
         team1.player2 = players[key];
+        team1.player2.cards = deck2;
         counter++;
       } else if (counter == 2) {
         team2.player1 = players[key];
+        team2.player1.cards = deck3;
         counter++;
       } else if (counter == 3) {
         team2.player2 = players[key];
+        team2.player2.cards = deck4;
         counter++;
       } 
     });
   }
 
-  function initGameMember() {
+  // function drawAllCards() {
+  //   let index = 0;
+  //   let counter = 0;
+  //   let playerCount = 1;
 
+  //   let team1Ref = gameSessionRef.child('team1');
+  //   let team2Ref = gameSessionRef.child('team2');
+  //   let currentCards = {};
+
+  //   Object.keys(dbDeck).forEach((key) => {
+  //     if (index <= 9) {
+  //       currentCards.counter = key;
+  //     }
+  //     if (counter == 9) { 
+  //       counter = 0; 
+  //       if (playerCount == 1) {
+  //         team1Ref.child('player1').update{
+  //           cards = currentCards
+  //         }
+  //         playerCount++;
+  //       } else if (playerCount == 2) {
+  //         team1
+  //       }
+        
+  //     }
+  //   });
+  // }
+
+
+  //Function initiates watching variables changes for both host and members
+  function initRefs() {
+    //Updates local var playerTurnID everytime it gets updates in db
+    gameSessionRef.child('currentPlayer').on("value", (snapshot) => {
+      playerTurnID = snapshot.val() || "";
+    });
+
+
+    //deckRef = gameSessionRef.child('deck');
+    cardsRef = gameSessionRef.child('deck').child('cards');
+    //cardIndexRef = gameSessionRef.child('cardIndex');
+
+    //Updates local var dbDeck with databases deck, allows us to store draw cards
+    // cardsRef.on("value", (snapshot) => {
+    //   dbDeck = snapshot.val() || {}; 
+    //   cardNum = Object.keys(dbDeck).length;
+    //   //cardNum = Object.keys(dbDeck).length; //save number of cards in deck
+    //   console.log(dbDeck);
+    //   console.log("number of cards is: ");
+    //   console.log(cardNum);
+    //   if (cardNum != 0 && playerHand.numberOfCards < 5) {
+    //     //for (let i = 0; i < 5; i++) {
+    //       let cardRef = cardsRef.child((cardNum-1).toString());
+    //       console.log(cardRef.toString());
+    //       cardRef.get().then(function() {
+    //         return cardRef.once("value");
+    //       }).then(function(snapshot) {
+    //         let currentCard = snapshot.val(); //Player count before adding the user pressing this
+    //         console.log("card is: ");
+    //         console.log(snapshot.val());
+    //         playerHand.add(currentCard.suit, currentCard.value);
+    //       });
+          
+    //     //}
+    //   }
+    // });
+
+    // cardsRef.on('child_removed', () => {
+    //   cardNum = cardNum - 1;
+    //   console.log("number of cards is: ");
+    //   console.log(cardNum);
+    // });
+
+  }
+
+  function initGameMember() {
+    //initRefs();
+    playerHand = new Deck([]);
+    //console.log("Checking if deck is null");
+    //console.log(deck);
+    console.log(Object.keys(dbDeck).length);
+    if (Object.keys(dbDeck).length != 0) { //make sure deck isnt null
+      //let currentCardsRef = gameSessionRef.child('deck').child('cards');
+      console.log(cardsRef.toString());
+      //console.log(deckRef.child('cards').toString());
+
+      for (let i = 1; i < 6; i++) {
+        let cardRef = cardsRef.child((cardNum-i).toString());
+        console.log(cardRef.toString());
+        cardRef.get().then(function() {
+          return cardRef.once("value");
+        }).then(function(snapshot) {
+          let currentCard = snapshot.val(); //Player count before adding the user pressing this
+          console.log("card is: ");
+          console.log(snapshot.val());
+          playerHand.add(currentCard.suit, currentCard.value);
+          cardRef.remove().then(function() {
+            console.log("Remove succeeded.")
+          }); 
+        });
+      }
+
+      //deck.drawCards(5, playerHand);
+      console.log("Joined Player Hand");
+      console.log(playerHand);
+
+      //Update deck in database
+      //gameSessionRef.update({
+        //deck : deck
+      //})
+
+    }
   }
 
   //Use function to create teams in firebase once everyone has joined the game
@@ -89,22 +213,30 @@ joinGameBtn.addEventListener("click", function() {
     //Create deck in database and here constantly updating it using on value changes 
     //Set up html visuals handling the card back showing up, deck counter and player cards showingUp
     //Set up deck on child removed update card count
+    //initRefs();
 
-    //Create game deck
-    deck = new Deck(freshDeck());
-    deck.shuffle();
     //Create deck storing current cards on board;
     board = new Deck([]);
     //Create user's card deck
     playerHand = new Deck([]);
-    //Create board and deck in database
-    gameSessionRef.update({
-      deck
-    })
+    deck.drawCards(5, playerHand);
+    console.log("Host hand: ");
+    console.log(playerHand);
 
     
-    //Create teams 
-    createTeams();
+
+    //Set starting player - playing order will be team1 -p1, team1 -p2, team2 -p1, team2 -p2
+    //playerTurnID = team1.player1.playerId;    
+    //Store current player in database
+    gameSessionRef.update({
+      currentPlayer: team1.player1.playerId
+    })
+
+
+    // gameSessionRef.child('currentPlayer').on("value", (snapshot) => {
+    //   playerTurnID = snapshot.val();
+    // });
+
     console.log(team1);
     console.log(team2);
     //deck.drawCards(5, playerHand);
@@ -147,7 +279,37 @@ joinGameBtn.addEventListener("click", function() {
       }
       if (snapshot.val() == playerLimit) {
         console.log("START GAME");
-        initGameHost();
+        deck1 = new Deck([]);
+        deck2 = new Deck([]);
+        deck3 = new Deck([]);
+        deck4 = new Deck([]);
+        deck.drawCards(10, deck1);
+        deck.drawCards(10, deck2);
+        deck.drawCards(10, deck3);
+        deck.drawCards(10, deck4);
+
+        //Create deck
+        // gameSessionRef.update({
+        //   deck,
+        //   cardIndex: 0
+        // })
+
+        //Assign cards to players
+
+
+        //Create teams
+        initRefs();
+
+        //Create teams, vars team1 and team2 store them in db
+        //Also sets up cards
+        createTeams();
+        gameSessionRef.update({
+          team1: team1,
+          team2: team2
+        });
+
+        //drawAllCards();
+        //initGameHost();
       }
     })
 
@@ -173,9 +335,11 @@ joinGameBtn.addEventListener("click", function() {
   joinSessionInp.addEventListener("change", function() {
     //TODO Improve how we determine the sessionID is valid, right now it just makes sure its the right length for 1
     if (joinSessionInp.value.length == 20) {
-      let sessionRef = firebase.database().ref('gameSession/'+joinSessionInp.value);
+      //let sessionRef = firebase.database().ref('gameSession/'+joinSessionInp.value);
+      gameSessionRef = firebase.database().ref('gameSession/'+joinSessionInp.value);
       let gameSessionJoinRef = firebase.database().ref('gameSession/'+joinSessionInp.value+'/sessionPlayers');
       let playerCountRef = firebase.database().ref('gameSession/'+joinSessionInp.value+'/playerCount');
+
 
       //Display text for user
       const para1 = document.createElement("p");
@@ -195,7 +359,8 @@ joinGameBtn.addEventListener("click", function() {
         }
         if (snapshot.val() == playerLimit) {
           console.log("START GAME");
-          initGameMember();
+          initRefs();
+          //initGameMember();
       }
       })
 
@@ -211,7 +376,7 @@ joinGameBtn.addEventListener("click", function() {
           playerId: playerId
         });
         //Update player count
-        sessionRef.update({
+        gameSessionRef.update({
           playerCount: currentPlayerCount + 1
         })
       } else {
